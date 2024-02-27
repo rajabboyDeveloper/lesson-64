2cbb06b2408aaa3e1e3402554a0e1d935ab26a22
rajabboyDeveloper <rajabboy@internet.ru>
2024-02-27 18:01:21 +0500

    i am changed my header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import './Header.css'
 function Header() {
   return (
-    <div className='Header'></div>
+    <div className='Header'>
+      <h1 className='title'>Helloooo guys !!!</h1>
+    </div>
   )
 }
 
